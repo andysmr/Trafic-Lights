e7e514e33504418728b04afde5ef2e2aebceb14e
andySmr <andrey180696007@yandex.ru>
2021-12-22 17:44:15 +0400

    version 1.1

--- Trafic Lights/ViewController.swift
+++ Trafic Lights/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var red: UIView!
+    @IBOutlet var yellow: UIView!
+    @IBOutlet var green: UIView!
+    @IBOutlet var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeActionButton() {
+    }
+    
+    
 }
 
